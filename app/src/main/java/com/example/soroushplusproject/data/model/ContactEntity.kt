a2e17a83d340c156eb3f297dc439359589d4bea1
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-16 19:13:49 +0330

    impl entity

--- app/src/main/java/com/example/soroushplusproject/data/model/ContactEntity.kt
+++ app/src/main/java/com/example/soroushplusproject/data/model/ContactEntity.kt
@@ -1,9 +1,20 @@
 package com.example.soroushplusproject.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity("contact_table")
 data class ContactEntity(
+    @ColumnInfo("id")
+    @PrimaryKey(false)
     val id: Int?,
+    @ColumnInfo("name")
     val name: String?,
+    @ColumnInfo("number")
     val number: String?,
+    @ColumnInfo("email")
     val Email: String?,
+    @ColumnInfo("image")
     val image: String?,
 )
