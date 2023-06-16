8d157d2d6a4f3941959b95484c772b54ae11bfc7
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-16 18:10:01 +0330

    impl ContactDetails

---
+++ app/src/main/java/com/example/soroushplusproject/ui/models/ContactDetails.kt
@@ -0,0 +1,8 @@
+package com.example.soroushplusproject.ui.models
+
+data class ContactDetails(
+    val name : String?,
+    val number : String?,
+    val email : String?,
+    val image : String?,
+)
