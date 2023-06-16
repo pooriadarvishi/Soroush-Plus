340c2dc632d138eb37f06a998aad907c91b3427a
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-16 17:12:40 +0330

    impl PermissionViewModel

--- app/src/main/java/com/example/soroushplusproject/ui/permission/PermissionViewModel.kt
+++ app/src/main/java/com/example/soroushplusproject/ui/permission/PermissionViewModel.kt
@@ -1,7 +1,19 @@
 package com.example.soroushplusproject.ui.permission
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class PermissionViewModel : ViewModel() {
+    private val _permissionState = MutableLiveData(false)
+    val permissionState: LiveData<Boolean> = _permissionState
 
+
+    fun permissionDenied() {
+        _permissionState.postValue(false)
+    }
+
+    fun permissionGranted() {
+        _permissionState.postValue(true)
+    }
 }
