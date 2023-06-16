04ce671bbc0d940cc950b02629e7bb89eaa716ee
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-16 18:13:07 +0330

    impl ContactEntity

---
+++ app/src/main/java/com/example/soroushplusproject/data/model/ContactEntity.kt
@@ -0,0 +1,9 @@
+package com.example.soroushplusproject.data.model
+
+data class ContactEntity(
+    val id: Int?,
+    val name: String?,
+    val number: String?,
+    val Email: String?,
+    val image: String?,
+)
