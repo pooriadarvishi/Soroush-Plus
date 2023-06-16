f9e573ea5f17f99fec67f0685432443cd1658e37
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-16 19:27:42 +0330

    impl AppModule

---
+++ app/src/main/java/com/example/soroushplusproject/di/AppModule.kt
@@ -0,0 +1,25 @@
+package com.example.soroushplusproject.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.soroushplusproject.data.local.ContactDataBase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideDataBase(application: Application): ContactDataBase =
+        Room.databaseBuilder(application, ContactDataBase::class.java, "contact_database").build()
+
+    @Provides
+    @Singleton
+    fun provideDao(contactDataBase: ContactDataBase) = contactDataBase.contactDao()
+}
