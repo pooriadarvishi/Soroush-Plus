79ec457790921c2548d866b7914bf6e11a620d2d
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-17 21:53:00 +0330

    refactor ContactProvider for clean code and debug

--- app/src/main/java/com/example/soroushplusproject/data/contents/ContactProvider.kt
+++ app/src/main/java/com/example/soroushplusproject/data/contents/ContactProvider.kt
@@ -10,41 +10,79 @@ class ContactProvider(private val context: Context) {
 
     @SuppressLint("Range")
     fun getContacts(): List<ContactEntity> {
-        val contacts = mutableListOf<ContactEntity>()
         val contentResolver: ContentResolver = context.contentResolver
+        return setContactInfo(contentResolver)
+    }
 
+    private fun setContactInfo(contentResolver: ContentResolver): MutableList<ContactEntity> {
+        val contacts = mutableListOf<ContactEntity>()
         contentResolver.query(
-            ContactsContract.CommonDataKinds.Phone.CONTENT_URI, arrayOf(
-                ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
-                ContactsContract.CommonDataKinds.Phone.NUMBER,
-                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                ContactsContract.CommonDataKinds.Phone.HAS_PHONE_NUMBER,
-                ContactsContract.CommonDataKinds.Email.ADDRESS
-            ), null, null, null
+            ContactsContract.Contacts.CONTENT_URI, null, null, null, null
         )?.use { cursor ->
-            val contactIdIndex =
-                cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID)
-            val numberIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
-            val nameIndex =
-                cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
-            val hasPhoneNumberIndex =
-                cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.HAS_PHONE_NUMBER)
-            val emailIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS)
-
+            val contactIdIndex = cursor.getColumnIndex(ContactsContract.Contacts._ID)
+            val nameIndex = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+            val photoIndex = cursor.getColumnIndex(ContactsContract.Contacts.PHOTO_URI)
             while (cursor.moveToNext()) {
                 val contact = ContactEntity().apply {
-                    id = cursor.getString(contactIdIndex).toInt()
-                    name = cursor.getString(nameIndex)
-                    number = cursor.getString(numberIndex)
-                    if (cursor.getInt(hasPhoneNumberIndex) > 0) {
-                        email = cursor.getString(emailIndex)
-                    }
-                }
+                    val sId = cursor.getString(contactIdIndex)
 
+                    name = cursor.getString(nameIndex)
+                    image = cursor.getString(photoIndex)
+
+                    setContactNumber(sId, this, contentResolver)
+
+                    setContactEmail(sId, this, contentResolver)
+
+                    id = sId.toInt()
+                }
                 contacts.add(contact)
             }
         }
-
         return contacts
     }
+
+    private fun setContactNumber(
+        id: String, contact: ContactEntity, contentResolver: ContentResolver
+    ) {
+        contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            null,
+            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+            arrayOf(id),
+            null
+        )?.use { phoneCursor ->
+            val hasPhoneNumberIndex =
+                phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.HAS_PHONE_NUMBER)
+            val numberIndex =
+                phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+            while (phoneCursor.moveToNext()) {
+                if (phoneCursor.getInt(hasPhoneNumberIndex) > 0) {
+                    contact.number = phoneCursor.getString(numberIndex)
+                }
+            }
+            phoneCursor.close()
+        }
+
+    }
+
+    private fun setContactEmail(
+        id: String, contact: ContactEntity, contentResolver: ContentResolver
+    ) {
+
+        contentResolver.query(
+            ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+            null,
+            ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+            arrayOf(id),
+            null
+        )?.use { emailCursor ->
+            val emailIndex =
+                emailCursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS)
+            while (emailCursor.moveToNext()) {
+                contact.email = emailCursor.getString(emailIndex)
+            }
+        }
+    }
+
+
 }
