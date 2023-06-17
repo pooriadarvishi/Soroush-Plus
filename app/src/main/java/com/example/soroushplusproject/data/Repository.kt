987cc6a9b763292b07b6d3ae7214abf14e702aee
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-17 20:15:14 +0330

    inject contactProvider into Repository

--- app/src/main/java/com/example/soroushplusproject/data/Repository.kt
+++ app/src/main/java/com/example/soroushplusproject/data/Repository.kt
@@ -1,10 +1,11 @@
 package com.example.soroushplusproject.data
 
+import com.example.soroushplusproject.data.contents.ContactProvider
 import com.example.soroushplusproject.data.local.ContactDao
 import com.example.soroushplusproject.data.model.ContactEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class Repository @Inject constructor(private val contactDao: ContactDao) {
+class Repository @Inject constructor(private val contactDao: ContactDao, private val contactProvider: ContactProvider){
 
 }
