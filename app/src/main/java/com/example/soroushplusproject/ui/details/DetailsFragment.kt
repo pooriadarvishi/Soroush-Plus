b44f9fb313bee1aa2397e39d18ccbb001c1a6f8d
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-17 23:24:13 +0330

    add DetailsFragment

---
+++ app/src/main/java/com/example/soroushplusproject/ui/details/DetailsFragment.kt
@@ -0,0 +1,14 @@
+package com.example.soroushplusproject.ui.details
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.soroushplusproject.R
+
+class DetailsFragment : Fragment() {
+
+
+}
---
+++ app/src/main/java/com/example/soroushplusproject/ui/details/DetailsViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.soroushplusproject.ui.details
+
+import androidx.lifecycle.ViewModel
+
+class DetailsViewModel : ViewModel() {
+}
