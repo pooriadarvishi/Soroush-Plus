c66018339d693d3a35f2961478fe855933b26461
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-17 23:27:32 +0330

    impl DetailsFragment

--- app/src/main/java/com/example/soroushplusproject/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/soroushplusproject/ui/details/DetailsFragment.kt
@@ -1,14 +1,26 @@
 package com.example.soroushplusproject.ui.details
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.soroushplusproject.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.soroushplusproject.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
 
+    private lateinit var binding: FragmentDetailsBinding
+    private val detailsViewModel: DetailsViewModel by viewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentDetailsBinding.inflate(inflater)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
