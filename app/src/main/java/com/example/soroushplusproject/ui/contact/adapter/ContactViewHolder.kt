f9b08f7b980a2f8926b4ceb18f510833d6289f81
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-19 19:32:21 +0330

    refactor beautiful contact_item_re

--- app/src/main/java/com/example/soroushplusproject/ui/contact/adapter/ContactViewHolder.kt
+++ app/src/main/java/com/example/soroushplusproject/ui/contact/adapter/ContactViewHolder.kt
@@ -1,8 +1,10 @@
 package com.example.soroushplusproject.ui.contact.adapter
 
+import androidx.core.view.isInvisible
 import androidx.recyclerview.widget.RecyclerView
 import com.example.soroushplusproject.databinding.ContactItemReBinding
 import com.example.soroushplusproject.ui.models.ContactItem
+import com.example.soroushplusproject.util.asFirst
 import com.example.soroushplusproject.util.loadImage
 
 class ContactViewHolder(private val binding: ContactItemReBinding, onClick: onClickContact) :
@@ -18,7 +20,11 @@ class ContactViewHolder(private val binding: ContactItemReBinding, onClick: onCl
         contact = contactItem
         binding.apply {
             tvContactName.text = contact.name
-            contactItem.image?.let { igContactProfile.loadImage(it) }
+            if (!contact.name.isNullOrEmpty()) tvName.text = contact.name?.asFirst()
+            contact.image?.let {
+                igContactProfile.loadImage(it)
+                binding.tvName.isInvisible = true
+            }
         }
     }
 }
