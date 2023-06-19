5637f8659e7b9d7eaa91b92a384eae8641fdd00f
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-19 11:29:30 +0330

    impl GetAllContactUseCase

---
+++ app/src/main/java/com/example/soroushplusproject/domain/GetAllContactUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.soroushplusproject.domain
+
+import com.example.soroushplusproject.data.Repository
+import com.example.soroushplusproject.domain.interact_result.InteractResult
+import com.example.soroushplusproject.ui.models.ContactItem
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetAllContactUseCase @Inject constructor(private val repository: Repository) :
+    InteractResult<List<ContactItem>>() {
+    override fun doWork(): Flow<List<ContactItem>> = repository.getAllContact()
+}
--- app/src/main/java/com/example/soroushplusproject/domain/interact_result/InteractResult.kt
+++ app/src/main/java/com/example/soroushplusproject/domain/interact_result/InteractResult.kt
@@ -5,12 +5,12 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 
-abstract class InteractResult<P, Q> {
-    operator fun invoke(params: P): Flow<InteractResultState<Q>> = doWork(params).map { result ->
+abstract class InteractResult<T> {
+    operator fun invoke(): Flow<InteractResultState<T>> = doWork().map { result ->
         if (result != null) InteractResultState.Success(result)
         else InteractResultState.Empty
     }.onStart { emit(InteractResultState.Loading) }.catch { emit(InteractResultState.Error) }
 
 
-    protected abstract fun doWork(params: P): Flow<Q>
+    protected abstract fun doWork(): Flow<T>
 }
