9aca4f7130801e73e4b85f624283ca586eb985ed
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-20 02:21:00 +0330

    impl baseFrag

---
+++ app/src/main/java/com/example/soroushplusproject/ui/base/BaseFragment.kt
@@ -0,0 +1,39 @@
+package com.example.soroushplusproject.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : ViewDataBinding>(@LayoutRes private val layoutResId: Int) :
+    Fragment() {
+
+    private var _binding: T? = null
+    val binding: T get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        return _binding!!.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUi()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    abstract fun setUi()
+}
