191b8cfb1e30b6cb45693e9d3212c4d00f731d64
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-19 17:20:52 +0330

    impl empty state

--- app/src/main/java/com/example/soroushplusproject/domain/interact_result/InteractResult.kt
+++ app/src/main/java/com/example/soroushplusproject/domain/interact_result/InteractResult.kt
@@ -1,6 +1,5 @@
 package com.example.soroushplusproject.domain.interact_result
 
-import android.util.Log
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
@@ -8,12 +7,24 @@ import kotlinx.coroutines.flow.onStart
 
 abstract class InteractResult<T,Q> {
     operator fun invoke(params: Q): Flow<InteractResultState<T>> = doWork(params).map { result ->
-        if (result != null) {
-            InteractResultState.Success(result)
+
+        when (result) {
+            is Collection<*> -> {
+                if (result.isEmpty()) {
+                    InteractResultState.Empty
+                } else {
+                    InteractResultState.Success(result)
+                }
+            }
+            else -> {
+                if (result != null) {
+                    InteractResultState.Success(result)
+                } else {
+                    InteractResultState.Empty
+                }
+            }
         }
-        else {
-            InteractResultState.Empty
-        }
+
     }.onStart { emit(InteractResultState.Loading) }.catch {
         emit(InteractResultState.Error)
     }
