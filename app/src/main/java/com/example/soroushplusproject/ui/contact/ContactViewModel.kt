74195439c7e123235312c5e373ab70feb56b4530
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-19 11:47:07 +0330

    impl useCases in ContactViewModel

--- app/src/main/java/com/example/soroushplusproject/ui/contact/ContactViewModel.kt
+++ app/src/main/java/com/example/soroushplusproject/ui/contact/ContactViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.soroushplusproject.data.Repository
+import com.example.soroushplusproject.domain.interact_result.InteractResultState
+import com.example.soroushplusproject.domain.interactors.GetAllContactUseCase
+import com.example.soroushplusproject.domain.interactors.SyncContactsUseCase
 import com.example.soroushplusproject.ui.models.ContactItem
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.collectLatest
@@ -13,9 +15,12 @@ import javax.inject.Inject
 
 
 @HiltViewModel
-class ContactViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
-    private val _contacts = MutableLiveData<List<ContactItem>>()
-    val contacts: LiveData<List<ContactItem>> = _contacts
+class ContactViewModel @Inject constructor(
+    private val getAllContactUseCase: GetAllContactUseCase,
+    private val syncContactsUseCase: SyncContactsUseCase
+) : ViewModel() {
+    private val _contacts = MutableLiveData<InteractResultState<List<ContactItem>>>()
+    val contacts: LiveData<InteractResultState<List<ContactItem>>> = _contacts
 
 
     init {
@@ -24,7 +29,7 @@ class ContactViewModel @Inject constructor(private val repository: Repository) :
 
     private fun getContacts() {
         viewModelScope.launch {
-            repository.getAllContact().collectLatest {
+            getAllContactUseCase(Unit).collectLatest {
                 _contacts.postValue(it)
             }
         }
@@ -32,7 +37,7 @@ class ContactViewModel @Inject constructor(private val repository: Repository) :
 
     fun sync() {
         viewModelScope.launch {
-            repository.syncContacts()
+            syncContactsUseCase(Unit)
         }
     }
 }
