a0bce03190134cf0e7a069ab4c7e25193c1da009
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-21 00:51:24 +0330

    impl service

--- app/src/main/java/com/example/soroushplusproject/data/services/ContactService.kt
+++ app/src/main/java/com/example/soroushplusproject/data/services/ContactService.kt
@@ -3,18 +3,37 @@ package com.example.soroushplusproject.data.services
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.provider.ContactsContract
+import com.example.soroushplusproject.data.contents_provider.ContentObserver
+import javax.inject.Inject
 
-class ContactService : Service() {
+class ContactService @Inject constructor(private val contentObserver: ContentObserver) : Service() {
     override fun onBind(intent: Intent?): IBinder? = null
 
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        observeContacts()
         return super.onStartCommand(intent, flags, startId)
-
     }
 
 
     override fun onDestroy() {
         super.onDestroy()
+        unObserveContact()
+    }
+
+
+    private fun observeContacts() {
+        val resolver = contentResolver
+        resolver.registerContentObserver(
+            ContactsContract.DeletedContacts.CONTENT_URI, true, contentObserver
+        )
+    }
+
+    private fun unObserveContact() {
+        val resolver = contentResolver
+        resolver.unregisterContentObserver(
+            contentObserver
+        )
     }
 }
